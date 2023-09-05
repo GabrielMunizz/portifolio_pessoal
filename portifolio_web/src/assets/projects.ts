c5f24990096a7dcd1eeb4fd776bfdfd1bcd4f8fa
Gabriel Muniz <gabrielmfd@gmail.com>
2023-09-05 06:54:03 -0300

    Cria arquivo projects.ts.

---
+++ portifolio_web/src/assets/projects.ts
@@ -0,0 +1,8 @@
+export const projects = [
+  {
+    id: 0,
+    title: 'Rick and Mortypedia',
+    url: '',
+    description: "My first personal project in React, using Rick and Morty's API. The application consists in a Rick and Morty's 'Wikipedia' and includes a memory game.  React Router Dom, useState, useEffect, Styled-Components are some of the skills used in this project."
+  }
+]
