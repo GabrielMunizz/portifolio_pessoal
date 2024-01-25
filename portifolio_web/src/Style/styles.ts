557e61c23a3d340d90ecc7698acd8c6c509750fd
Gabriel Muniz <gabrielmfd@gmail.com>
2024-01-25 10:27:23 -0300

    Style: cria arquivo próprio para a estilização da page Contact

--- portifolio_web/src/Style/styles.ts
+++ portifolio_web/src/Style/styles.ts
@@ -16,28 +16,3 @@ export const H2 = styled.h2`
     font-size: 14px;    
   }
 `;
-
-export const ContactSection = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  width: 400px;
-  height: 200px;
-  border: 1px solid ${(props) => props.theme.colors.main_text_color};
-
-  & span {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-  }
-
-  & div h4 {
-    margin: 8px;
-  }
-
-  & div a {
-    color: ${(props) => props.theme.colors.text_details};
-  }
-`;
--- portifolio_web/src/pages/Contact.tsx
+++ portifolio_web/src/pages/Contact.tsx
@@ -1,19 +1,21 @@
-import * as S from '../Style/styles';
-import { Main } from '../Style/mainStyle';
+import * as S from '../Style/contactStyle';
+import { H2 } from '../Style/mainStyle';
 import { AiFillContacts } from 'react-icons/ai';
 
 const Contact = () => {
   return(
-    <Main>
+    <S.Contact>
       <S.ContactSection>
-        <S.H2><span><AiFillContacts />Contact:</span></S.H2>
+        <H2>
+          <span><AiFillContacts />Contact:</span>
+        </H2>
         <div>
           <h4>E-mail: gabrielmfd@gmail.com</h4>
           <h4>LinkedIn: <a href="https://www.linkedin.com/in/gabriel-muniz-dev/" target='_blank'>gabriel-muniz-dev</a></h4>
           <h4>Phone: +55 32 98486-3437</h4>
         </div>
       </S.ContactSection>
-    </Main>
+    </S.Contact>
   )
 }
 
