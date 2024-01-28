121df6b1fe9858a33b8c6f9dfbba7bdb5b9bb7da
Gabriel Muniz <gabrielmfd@gmail.com>
2024-01-28 17:49:19 -0300

    Feat: adiciona texto em português na page FullBio

---
+++ src/assets/fullBioTxt.ts
@@ -0,0 +1,30 @@
+export const fullBioTxt = `After 10 years working in business administration area, 
+I've decided to explore my passion for technology and embark on a new professional journey. 
+In 2019, I've made a decision to take a break and reflect on my goals and interests.
+During this period, I've realized that my affinity for technology, especially through gaming, could be more than just a hobby. 
+With a lot of determination, I immersed myself in independent studies and became even more fascinated by the world of programming.
+
+Since then, I've decided to embrace this opportunity and fully dive into the programming world.
+Today, I'm excited to leverage my background in administration and my passion for technology to add value to an innovative company. 
+I'm ready to face challenges, learn continuously, and contribute to projects and team growth.
+If you're looking for a dedicated and versatile professional who is passionate about technology, let's talk! 
+I'm open to opportunities that allows me to apply my knowledge and contribute to the success of challenging projects.`
+
+export const fullBioTxtPT = `Mesmo antes de me formar em Administração de Empresas, lá em 2013, já trabalhava na área como assistente administrativo. Trabalhei muito, me esforcei e consegui ter meu próprio negócio, só para perceber que não era o que eu queria fazer da minha vida.
+
+Foi um pouco assustador me dar conta disso, principalmente depois dos 30 anos de idade. Então bateu aquele frio na barriga com a ideia de começar uma carreira do zero novamente. Mas me ocorreu que, se não tomasse essa decisão, iria me arrepender pelo resto da vida. Porém, depois dessa decisão, vinha outro problema: O que fazer? Qual área seguir? Tinha que ser algo que fosse levar para a vida.
+
+Resolver quebra-cabeças sempre foi uma das coisas que mais gostei de fazer. Palavras-cruzadas, desvendar mistérios em livros, descobrir quem é o vilão em séries... Mas nunca tinha relacionado essas coisas com resolução de problemas, ou sequer programação.
+
+Porém, quando você realmente quer alguma coisa, a vida tende a te colocar no caminho certo. Já aconteceu comigo algumas vezes. E, dessa vez, não foi diferente. Ao navegar pela internet, me deparei com um vídeo de uma pessoa que mudou de carreira depois dos 30 anos e tinha conseguido aprender programação em desenvolvimento web sozinha, fazendo cursos grátis online e assistindo vídeos. Aquilo abriu minha mente para procurar mais sobre o tema de programação, mais especificamente o JavaScript.
+
+Comecei a fazer um curso grátis de JavaScript no www.freecodecamp.org e vi onde minha paixão por quebra-cabeças podia se encaixar na minha vida profissional: Programação. Desde então, foi um caminho sem volta. Terminei o curso do freeCodeCamp, comecei a fazer um curso na RocketSeat, onde aprendi mais sobre HTML e CSS, depois fui para a Trybe, onde pude me aprofundar realmente em desenvolvimento web.
+
+Após aprender sobre Front-End com React, Redux, Styled-Components, estou estudando Back-End com Node.JS e Sequelize (também dou uma estudada em Go nas horas vagas). Meu objetivo é atuar como desenvolvedor Full-Stack. Graças a mais de 10 anos em administração, trabalhei muito com pessoas e pude desenvolver bastante a minha comunicação, trabalho em equipe e soft-skills. Percebi que ser objetivo e claro no mundo da programação pode evitar muitos bugs.
+
+O que mais aprendi nessa jornada é que não se pode desistir dos sonhos e muito menos achar que é tarde para recomeçar. É melhor começar tarde do que nunca. A vida passa rápido, portanto que seja prazerosa em todos os aspectos!
+
+Se leu até aqui, muito obrigado pelo seu tempo.
+
+Gabriel Muniz
+`
