6fd576efe5016e68633db137924f325d31c4b4ee
Gabriel Muniz <gabrielmfd@gmail.com>
2024-04-24 10:41:10 -0300

    Altera Front-end para Full-stack na Home

--- src/assets/applicationTexts.ts
+++ src/assets/applicationTexts.ts
@@ -2,14 +2,14 @@ import { ApplicationTxtType } from './types'
 
 const eVersion: ApplicationTxtType = {
   professional: [
-    '- Front-End Web Developer',
+    '- Full-Stack Web Developer',
     '- Guitar Enthusiast',
     '- Food Alchemist',
   ],
   contact: 'CONTACT ME',
   intro: {
     head: 'Introduction:',
-    title: 'Front-End Web Developer',
+    title: 'Full-Stack Web Developer',
     bioPreview: `Even before I graduate in Business Administration, back in 2013, I've already worked in the area as a administrative assistant.
     Worked hard, pushed myself and managed to have my own business, just to realize that it wasn't what I wanted to do for the rest
     of my life...`
@@ -23,14 +23,14 @@ const eVersion: ApplicationTxtType = {
 
 const ptVersion: ApplicationTxtType = {
   professional: [
-    '- Desenvolvedor Web Front-End',
+    '- Desenvolvedor Web Full-Stack',
     '- Entusiasta na Guitarra',
     '- Alquimista na Cozinha',
   ],
   contact: 'CONTATO',
   intro: {
     head: 'Introdução:',
-    title: 'Desenvolvedor Web Front-End',
+    title: 'Desenvolvedor Web Full-Stack',
     bioPreview: `Mesmo antes de me formar em Administração de Empresas, lá em 2013, já trabalhava na área como assistente administrativo.
     Trabalhei muito, me esforcei e consegui ter meu próprio negócio, só para perceber que não era o que eu queria fazer da minha vida...`
   },
